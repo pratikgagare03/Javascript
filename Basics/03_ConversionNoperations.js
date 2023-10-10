d037a58ff424661cf215febcfcb9ec8ab6fbc17e
Pratik Gagare <89626617+pratikgagare03@users.noreply.github.com>
2023-10-10 13:36:25 +0000

    Summary of dataTypes

--- Basics/03_ConversionNoperations.js
+++ Basics/03_ConversionNoperations.js
@@ -63,7 +63,7 @@ num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
 ++gameCounter;
-console.log(gameCounter);
+// console.log(gameCounter)
 
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
