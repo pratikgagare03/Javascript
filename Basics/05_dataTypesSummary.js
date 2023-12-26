6bd4e060e07fe8f0975386a27f6cae82e91e08e4
Pratik Gagare <89626617+pratikgagare03@users.noreply.github.com>
2023-12-26 06:29:04 +0000

    Memory in Js

--- Basics/05_dataTypesSummary.js
+++ Basics/05_dataTypesSummary.js
@@ -38,3 +38,33 @@ const myFunction = function(){
 // console.log(typeof anotherId); //symbol
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// stack(primitive), Heap(Non-Primitive)
+
+let userOne = 123
+let userTwo = userOne
+console.log(userTwo);
+
+userTwo = 456
+
+console.log(userOne);
+console.log(userTwo); // only change in second variable as it is pass by value
+
+
+
+let userObjOne = {
+    name: "Pratik",
+    id:1
+}
+
+let userObjTwo = userObjOne;
+userObjTwo.name = "Rutik";
+
+console.log(userObjOne.name);
+console.log(userObjTwo.name); // Both changes as it is pass by reference
+
+
+
